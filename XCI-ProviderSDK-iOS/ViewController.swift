b1fdd7fd4246aaa9272b168bd70ec27861f71dc1
Sawyer Billings <sawpaw19@gmail.com>
2019-01-31 13:26:40 -0500

    Initial Commit

---
+++ XCI-ProviderSDK-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XCI-ProviderSDK-iOS
+//
+//  Created by Sawyer Billings on 1/31/19.
+//  Copyright © 2019 Rightpoint. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
