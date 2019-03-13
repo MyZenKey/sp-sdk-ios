3a2823498928db3e7c65e70e619ac87667c7c78a
Adam Tierney <adam.tierney@raizlabs.com>
2019-03-13 13:28:36 -0700

    - pr feedback

--- CarriersSharedAPI/Sources/AuthorizationError.swift
+++ CarriersSharedAPI/Sources/AuthorizationError.swift
@@ -19,6 +19,8 @@ public enum AuthorizationError: Error {
 }
 
 /// OAuth 2.0 Error Codes
+///
+/// for additional info, see: [OAuth 2.0 Authorization Section 4.2.2.1](https://tools.ietf.org/html/rfc6749#page-35)
 public enum OAuthErrorCode: String {
     /// The RP has constructed a bad request. This may be due to any of the parameters submitted.
     /// The RP should visit the service portal to confirm the parameters they should use.
@@ -28,24 +30,26 @@ public enum OAuthErrorCode: String {
     case unauthorizedClient = "unauthorized_client"
     /// The RP has constructed a bad request. This may be due to any of the parameters submitted.
     /// The RP should only use code or async-token.
-    case unsupportedResponseType = "unsupported_responce_type"
+    case unsupportedResponseType = "unsupported_response_type"
     /// The RP has constructed a bad request. The RP should visit the service portal to confirm the
     /// scopes allowed
     case invalidScope = "invalid_scope"
     case serverError = "server_error"
     case temporarilyUnavailable = "temporarily_unavailable"
+    /// The resource owner or authorization server denied the request.
+    ///
+    /// - SeeAlso: `ProjectVerifyErrorCode.requestDenied`
+    case accessDenied = "access_denied"
 }
 
 /// Open Id Error Codes
+///
+/// for additional info, see [OpenId Connect Core Section 3.1.2.6](https://openid.net/specs/openid-connect-core-1_0.html#AuthError)
 public enum OpenIdErrorCode: String {
-    /// The RP should not be using display=none
+    /// The RP should not be using `display=none`
     case interactionRequired = "interaction_required"
-    /// The RP should not be using display=none
+    /// The RP should not be using `display=none`
     case loginRequired = "login_required"
-    /// This error should never be returned.
-    case accountSelectionRequired = "account_selection_required"
-    /// This error should never be returned.
-    case consentRequired = "consent_required"
     case invalidRequestURI = "invalid_request_uri"
     /// This error may be returned while carriers are still adding support for Request objects
     case invalidRequestObject = "invalid_request_object"
@@ -55,6 +59,10 @@ public enum OpenIdErrorCode: String {
     case requestUIRNotSupported = "request_uri_not_supported"
     /// Dynamic registration will not be supported.
     case registrationNotSupported = "registration_not_supported"
+    /// This error should never be returned in the context of project verify.
+    case accountSelectionRequired = "account_selection_required"
+    /// This error should never be returned in the context of project verify.
+    case consentRequired = "consent_required"
 }
 
 /// Project Verify Error Codes
@@ -70,7 +78,7 @@ public enum ProjectVerifyErrorCode: String {
     /// The user may not have access to their phone and therefore the  transaction may have failed.
     /// Or the user did not notice the request.
     case authenticationTimedOut = "authentication_timed_out"
-    /// The users device has been unsuccessful with authentication. This may happen if the user has
-    /// changed sim cards, or just reset their device.
+    /// The user's device has been unsuccessful with authentication. This may happen if the user has
+    /// changed SIM cards, or just reset their device.
     case networkFailure = "network_failure"
 }
