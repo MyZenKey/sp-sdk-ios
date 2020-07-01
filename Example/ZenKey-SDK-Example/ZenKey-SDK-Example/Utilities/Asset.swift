562d77d8dacee9c31e5edb36487e5a6b8faa677f
minimusic <chad@minimusic.com>
2020-07-01 15:32:11 -0700

    fix copyright/license text

--- Example/ZenKey-SDK-Example/ZenKey-SDK-Example/Utilities/Asset.swift
+++ Example/ZenKey-SDK-Example/ZenKey-SDK-Example/Utilities/Asset.swift
@@ -3,7 +3,19 @@
 //  ZenKey-SDK-Example
 //
 //  Created by Chad Mealey on 4/17/20.
-//  Copyright © 2020 Rightpoint. All rights reserved.
+//  Copyright © 2020 ZenKey, LLC.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 import Foundation
